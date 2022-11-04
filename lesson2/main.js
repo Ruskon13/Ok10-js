471fe97222c21130344d3ac7ca29e79b2f10f33d
Ruskon13 <ruslankondra14@gmail.com>
2022-11-04 15:17:01 +0200

    Commit hmw2 js

---
+++ lesson2/main.js
@@ -0,0 +1,156 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+const carCompanies = []
+
+carCompanies[1] = 1905
+carCompanies[5] = 'name6'
+carCompanies[9] = true
+
+// console.log(carCompanies[1]);
+// console.log(carCompanies[5]);
+// console.log(carCompanies[9]);
+
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+let book = [
+    {
+        title: 'Kingdom',
+        pageCount: 220,
+        genre: 'Fairy tale'
+    }
+]
+// console.log(book);
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors.
+// Поле "автори" - масив. Кожен автор має поля name,age
+
+let description = [
+    {
+        title: 'NAME_TITLE',
+        pageCount: 190,
+        genre: 'Horor',
+        authors: [
+            {
+                name: 'Alex',
+                age:    35
+            },
+            {
+                name: 'Olya',
+                age:    38
+            }
+        ]
+    },
+    {
+        title: 'NAME_TITLE2',
+        pageCount: 200,
+        genre: 'IDK',
+        authors:  [
+            {
+                name: 'Alex2',
+                age:    40
+            },
+            {
+                name: 'Olya2',
+                age:    65
+            }
+        ]
+    },
+    {
+        title: 'NAME_TITLE3',
+        pageCount: 255,
+        genre: 'Comedy',
+        authors: [
+            {
+                name: 'Alex3',
+                age:    22
+            },
+            {
+                name: 'Olya3',
+                age:    15
+            }
+        ]
+    }
+
+]
+// console.log(description[1].authors[0].age);
+
+
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач".
+// Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+let users =[
+
+    {
+        name:'Name1',
+        username: 'username1',
+        password: 'password1'
+    },
+    {
+        name:'Name2',
+        username: 'username2',
+        password: 'password2'
+    },
+    {
+        name:'Name3',
+        username: 'username3',
+        password: 'password3'
+    },
+    {
+        name:'Name4',
+        username: 'username4',
+        password: 'password4'
+    },
+    {
+        name:'Name5',
+        username: 'username5',
+        password: 'password5'
+    },
+    {
+        name:'Name6',
+        username: 'username6',
+        password: 'password6'
+    },
+    {
+        name:'Name7',
+        username: 'username7',
+        password: 'password7'
+    },
+    {
+        name:'Name8',
+        username: 'username8',
+        password: 'password8'
+    },
+    {
+        name:'Name9',
+        username: 'username9',
+        password: 'password9'
+    },
+    {
+        name:'Name10',
+        username: 'username10',
+        password: 'password10'
+    }
+]
+
+// console.log(users[0].password);
+// console.log(users[1].password);
+// console.log(users[2].password);
+// console.log(users[3].password);
+// console.log(users[4].password);
+// console.log(users[5].password);
+// console.log(users[6].password);
+// console.log(users[7].password);
+// console.log(users[8].password);
+// console.log(users[9].password);
+
+
+
+
+
+
+
+
+
